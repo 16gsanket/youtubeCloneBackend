ad2ce96ae93e4313cad364c9f0d6043d92fe5d8b
16gsanket <16gsanket@gmail.com>
2024-05-13 11:42:51 +0530

    testing the local logut things

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {registerUser , loginUser} from "../controllers/user.controller.js";
+import {registerUser , loginUser, logoutUser} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
@@ -19,5 +19,7 @@ router.route("/register").post(
 );
 
 router.route("/login").post(loginUser)
+
+router.route('/logout').post(logoutUser)
 
 export { router };
